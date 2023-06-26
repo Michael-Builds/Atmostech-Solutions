18dd415f435e72caa02ea860753f00b0bb93a081
Kabanda Kpanti Michael <you@example.com>
2023-06-26 10:51:24 +0000

    Initial Commit

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Nav/Navbar'
+
+const Contact = () => {
+  return (
+    <>
+      <Navbar />
+      <div>Contact</div>
+    </>
+  );
+}
+
+export default Contact
---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Nav/Navbar'
+
+const Services = () => {
+  return (
+    <>
+      <Navbar />
+      <div>Services</div>
+    </>
+  );
+}
+
+export default Services
