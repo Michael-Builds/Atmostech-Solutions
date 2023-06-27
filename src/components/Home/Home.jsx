71b2f0db536ec5173a92351a1ab58dd04b9cd578
Kabanda Kpanti Michael <you@example.com>
2023-06-27 17:25:15 +0000

    Home Page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,6 +4,12 @@ import "./home.css";
 import { IoIosArrowForward } from "react-icons/io";
 import { IoIosArrowUp } from "react-icons/io";
 import { FaCog, FaLaptop } from "react-icons/fa";
+import partner1 from "../../assets/Institute of Engineering.png";
+import partner2 from "../../assets/Local Government Service.png";
+import partner3 from "../../assets/Ministry of Sanitation.png";
+import partner4 from "../../assets/Ministry of Works and Housing.png";
+import partner5 from "../../assets/RCD.png";
+import partner6 from "../../assets/Tamani.png";
 
 const Home = () => {
   const [showScrollToTop, setShowScrollToTop] = useState(false);
@@ -24,9 +30,61 @@ const Home = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
+  // Array of images for the banner which will change after every 3 seconds
+  // const banner = [
+  //   "image1.jpg",
+  //   "image2.jpg",
+  //   "image3.jpg",
+  //   "image4.jpg",
+  //   "image5.jpg",
+  //   "image6.jpg",
+  //   "image7.jpg",
+  //   "image8.jpg",
+  // ];
+
+  // Array of images for the value proposition which will change after every 3 seconds
+  // const value-left = [
+  //   "image1.jpg",
+  //   "image2.jpg",
+  //   "image3.jpg",
+  //   "image4.jpg",
+  //   "image5.jpg",
+  //   "image6.jpg",
+  //   "image7.jpg",
+  //   "image8.jpg",
+  //   "image9.jpg",
+  //   "image10.jpg",
+  //   "image11.jpg",
+  //   "image12.jpg",
+  //   "image13.jpg",
+  //   "image14.jpg",
+  //   "image15.jpg"
+  // ];
+
+  // Array of images for the value proposition which will change after every 3 seconds
+  // const value-right = [
+  //   "image1.jpg",
+  //   "image2.jpg",
+  //   "image3.jpg",
+  //   "image4.jpg",
+  //   "image5.jpg",
+  //   "image6.jpg",
+  //   "image7.jpg",
+  //   "image8.jpg",
+  //   "image9.jpg",
+  //   "image10.jpg",
+  //   "image11.jpg",
+  //   "image12.jpg",
+  //   "image13.jpg",
+  //   "image14.jpg",
+  //   "image15.jpg"
+  // ];
+
   return (
     <>
       <Navbar />
+
+      {/* Hero or Banner Section */}
       <section className="banner">
         {/* Slider Start */}
         <div className="container">
@@ -57,6 +115,56 @@ const Home = () => {
         </div>
       </section>
 
+      {/* Value proposition */}
+      <section className="value-proposition">
+        <div className="container">
+          <div className="value-proposition-container">
+            <div className="value-proposition-left">
+              <h2>
+                Unlock the Power of Technology with Our Cutting-Edge Solutions
+              </h2>
+              <p>
+                At Atmos Technology, we understand the transformative potential
+                of technology in today's digital landscape. Our cutting-edge
+                solutions are designed to empower businesses and individuals
+                with the tools and capabilities they need to thrive in the
+                modern world.
+              </p>
+              <p>
+                Our differentiating factor lies in our dedication to innovation
+                and quality. Through leveraging cutting-edge advancements in
+                technology, we provide tailored, state-of-the-art solutions to
+                meet your specific needs.
+              </p>
+
+              <p>
+                Embrace the digital revolution and unlock new opportunities with
+                our comprehensive range of tech solutions. Join the countless
+                businesses and individuals who have experienced the power of our
+                cutting-edge technology in driving growth and success.
+              </p>
+              {/* Image div for the value proposition-left */}
+              <div className="image-right"></div>
+            </div>
+
+            <div className="value-proposition-right">
+              <h2>Why Choose Our Tech Solutions?</h2>
+              <ul>
+                <li>Seamless integration with your existing systems</li>
+                <li>Scalable solutions that grow with your business</li>
+                <li>Advanced security measures to protect your data</li>
+                <li>Intuitive user interfaces for easy adoption</li>
+                <li>Proven track record of successful implementations</li>
+              </ul>
+
+              {/* Image div for the value proposition-right */}
+              <div className="image-left"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Service and Product Section */}
       <section id="services-products" className="wrapper style2">
         <div className="title">Our Services and Products</div>
         <div className="container">
@@ -126,70 +234,50 @@ const Home = () => {
         </div>
       </section>
 
-      {/* Value proposition */}
-      <section className="value-proposition">
-        <div className="container">
-          <div className="value-proposition-container">
-            <div className="value-proposition-left">
-              <h2>
-                Unlock the Power of Technology with Our Cutting-Edge Solutions
-              </h2>
-              <p>
-                At Atmos Technology, we understand the transformative potential
-                of technology in today's digital landscape. Our cutting-edge
-                solutions are designed to empower businesses and individuals
-                with the tools and capabilities they need to thrive in the
-                modern world.
-              </p>
-              <p>
-                Our differentiating factor lies in our dedication to innovation
-                and quality. Through leveraging cutting-edge advancements in
-                technology, we provide tailored, state-of-the-art solutions to
-                meet your specific needs.
-              </p>
-
-              <p>
-                Embrace the digital revolution and unlock new opportunities with
-                our comprehensive range of tech solutions. Join the countless
-                businesses and individuals who have experienced the power of our
-                cutting-edge technology in driving growth and success.
-              </p>
-            </div>
-
-            <div className="value-proposition-right">
-              <h2>Why Choose Our Tech Solutions?</h2>
-              <ul>
-                <li>Seamless integration with your existing systems</li>
-                <li>Scalable solutions that grow with your business</li>
-                <li>Advanced security measures to protect your data</li>
-                <li>Intuitive user interfaces for easy adoption</li>
-                <li>24/7 technical support and assistance</li>
-                <li>Proven track record of successful implementations</li>
-              </ul>
-            </div>
-            <div className="last-line">
-              {/* <h2> Come Over</h2>
-              <p className="last-line-text">
-                Elevate your business to new heights with Atmos Tech Solutions'
-                cutting-edge technology offerings. Uncover the transformative
-                impact our solutions can have on revolutionizing your operations
-                and fueling sustainable growth.
-              </p> */}
-              {/* <div className="btn-container">
-                <a
-                  href="appoinment.html"
-                  target="_blank"
-                  className="btn btn-main"
-                >
-                  Get in touch <IoIosArrowForward className="btn-icon" />
-                </a>
-              </div> */}
+      {/* Partner Section */}
+      <section class="partners-section">
+        <div class="partners-left">
+          <h2>Our Trusted Partners</h2>
+          <p>
+            We are proud to collaborate with industry-leading partners who share
+            our <br></br> vision and commitment to excellence.
+          </p>
+        </div>
+        <div class="partners-right">
+          <div className="partner-images">
+            <div className="partner-images">
+              <img src={partner1} alt="Partner 1" />
+              <img src={partner2} alt="Partner 2" />
+              <img src={partner3} alt="Partner 3" />
+              <img src={partner4} alt="Partner 4" />
+              <img src={partner5} alt="Partner 5" />
+              <img src={partner6} alt="Partner 6" />
             </div>
           </div>
         </div>
       </section>
 
-      
+      {/* Our team section */}
+      <section className="team-section">
+        <div className="team-up">
+          <h2>Our Team</h2>
+          <p>
+            Our team consists of highly skilled and dedicated professionals who
+            are passionate about delivering exceptional results.<br></br> We work
+            together, leveraging our diverse expertise and shared vision, to
+            achieve excellence in everything we do.
+          </p>
+        </div>
+        <div className="team-down">
+          <div className="team-container">
+            <div className="team-member"></div>
+            <div className="team-member"></div>
+            <div className="team-member"></div>
+            <div className="team-member"></div>
+            <div className="team-member"></div>
+          </div>
+        </div>
+      </section>
 
       {showScrollToTop && (
         <div className="scroll-to-top" onClick={scrollToTop}>
