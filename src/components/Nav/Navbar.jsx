70b9b5fec050b0512dbde963a3eadf0e0781eaa2
Kabanda Kpanti Michael <you@example.com>
2023-07-04 08:54:42 +0000

    Section Rearrangement

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,80 +1,68 @@
 import "./navbar.css";
 import React, { useState } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
-import { FaRegTimesCircle } from "react-icons/fa";
+import { VscChromeClose } from "react-icons/vsc";
 import { NavLink } from "react-router-dom";
 import { IoLocationOutline } from "react-icons/io5";
 import { HiOutlineMail } from "react-icons/hi";
 import { MdPhoneInTalk } from "react-icons/md";
-import { IoIosGlobe } from "react-icons/io";
+import { IoIosGlobe, IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
   const [selectedLanguage, setSelectedLanguage] = useState("en");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
   const handleLanguageSelect = (event) => {
     const selectedLanguage = event.target.value;
     setSelectedLanguage(selectedLanguage);
-
-    // Perform any translation logic here based on the selected language
-    if (selectedLanguage === "en") {
-      // Perform translation to English
-      translateToEnglish();
-    } else if (selectedLanguage === "fr") {
-      translateToFrench();
-    } else {
-      // Handle other languages
-      // ...
-    }
-  };
-
-  const translateToEnglish = () => {
-    const translations = {
-      home: "Home",
-      about: "About",
-      services: "Services",
-      contact: "Contact",
-    };
-
-    // Replace the text content or update the state with the translated values
-    document.getElementById("home-link").textContent = translations.home;
-    document.getElementById("about-link").textContent = translations.about;
-    document.getElementById("services-link").textContent =
-      translations.services;
-    document.getElementById("contact-link").textContent = translations.contact;
-  };
-
-  const translateToFrench = () => {
-    const translations = {
-      home: "Accueil",
-      about: "À propos",
-      services: "Services",
-      contact: "Contactez-nous",
-    };
-
-    // Replace the text content or update the state with the translated values
-    document.getElementById("home-link").textContent = translations.home;
-    document.getElementById("about-link").textContent = translations.about;
-    document.getElementById("services-link").textContent =
-      translations.services;
-    document.getElementById("contact-link").textContent = translations.contact;
   };
 
   const handleClick = () => {
     setClick(!click);
-    if (click) {
-      setClick(false);
-    }
   };
 
   const handleLinkClick = () => {
-    if (click) {
-      setClick(false);
-    }
+    setClick(false);
+    setIsDropdownOpen(false);
+  };
+
+  const handleMouseEnterServices = () => {
+    setIsDropdownOpen(true);
+  };
+
+  const handleMouseLeaveServices = () => {
+    setIsDropdownOpen(false);
   };
 
   const locationUrl =
     "https://www.google.com.gh/maps/place/VOLTA+ECG+CREDIT+UNION/@6.6112634,0.4602675,17z/data=!3m1!4b1!4m6!3m5!1s0x10272524c3e196fd:0x427847d77aecbb69!8m2!3d6.6112581!4d0.4628424!16s%2Fg%2F11f_j2vxhb?entry=ttu";
+
+  const languageOptions = [
+    { value: "en", label: "English" },
+    { value: "fr", label: "French" },
+  ];
+
+  const translations = {
+    en: {
+      home: "Home",
+      about: "About",
+      services: "Services",
+      contact: "Contact",
+    },
+    fr: {
+      home: "Accueil",
+      about: "À propos",
+      services: "Services",
+      contact: "Contactez-nous",
+    },
+  };
+
+  const arrowIcon = isDropdownOpen ? (
+    <IoIosArrowUp className="arrow-icon" />
+  ) : (
+    <IoIosArrowDown className="arrow-icon" />
+  );
 
   return (
     <header className="header">
@@ -114,38 +102,58 @@ const Navbar = () => {
           <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li>
               <NavLink
-                id="home-link"
                 to="/"
                 exact
                 activeClassName="active-link"
                 onClick={handleLinkClick}
               >
-                Home
+                {translations[selectedLanguage].home}
               </NavLink>
             </li>
             <li>
               <NavLink
-                id="about-link"
                 to="/about"
                 activeClassName="active-link"
                 onClick={handleLinkClick}
               >
-                About
+                {translations[selectedLanguage].about}
               </NavLink>
             </li>
             <li>
               <NavLink
-                id="services-link"
                 to="/service"
                 activeClassName="active-link"
                 onClick={handleLinkClick}
+                onMouseEnter={handleMouseEnterServices}
+                onMouseLeave={handleMouseLeaveServices}
               >
-                Services
+                {translations[selectedLanguage].services} {arrowIcon}
               </NavLink>
+              {isDropdownOpen && (
+                <ul className="dropdown-menu">
+                  <li>
+                    <NavLink
+                      to="/departments"
+                      activeClassName="active-link"
+                      onClick={handleLinkClick}
+                    >
+                      Web Development
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to="/department-single"
+                      activeClassName="active-link"
+                      onClick={handleLinkClick}
+                    >
+                      Mobile Development
+                    </NavLink>
+                  </li>
+                </ul>
+              )}
             </li>
             <li>
               <NavLink
-                id="industry-link"
                 to="/industry"
                 activeClassName="active-link"
                 onClick={handleLinkClick}
@@ -155,12 +163,11 @@ const Navbar = () => {
             </li>
             <li>
               <NavLink
-                id="contact-link"
                 to="/contact"
                 activeClassName="active-link"
                 onClick={handleLinkClick}
               >
-                Contact
+                {translations[selectedLanguage].contact}
               </NavLink>
             </li>
             <li className="language-selector">
@@ -170,9 +177,11 @@ const Navbar = () => {
                   value={selectedLanguage}
                   onChange={handleLanguageSelect}
                 >
-                  <option value="en">English</option>
-                  <option value="fr">French</option>
-                  {/* Add more options for other languages */}
+                  {languageOptions.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
                 </select>
                 <IoIosGlobe className="language-icon" />
               </div>
@@ -183,7 +192,7 @@ const Navbar = () => {
             onClick={handleClick}
           >
             {click ? (
-              <FaRegTimesCircle className="icon " />
+              <VscChromeClose className="icon " />
             ) : (
               <AiOutlineMenu className="icon" />
             )}
