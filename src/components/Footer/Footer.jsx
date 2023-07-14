1da2fde0161612fe9934cea166d6babf126ba929
Kabanda Kpanti Michael <you@example.com>
2023-07-14 17:58:10 +0000

    Footer Color Corrected

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -51,17 +51,17 @@ export default function Footer() {
                         <div key={key} className="w-full ">
                             <Typography
                                 variant="small"
-                                className="mb-4 font-bold uppercase opacity-100"
+                                className="mb-4 font-bold uppercase footer-main"
                             >
                                 {title}
                             </Typography>
                             <ul className="space-y-1">
                                 {links.map(({ title, path }, key) => (
-                                    <Typography key={key} as="li" color="blue-gray" className="font-normal">
+                                    <Typography key={key} as="li" className="font-normal">
                                         <NavLink
                                             to={path}
                                             activeClassName="text-green-500"
-                                            className="inline-block py-1 pr-2 transition-transform hover:scale-115"
+                                            className="inline-block py-1 pr-2 transition-transform hover:scale-115 footer-lil"
                                         >
                                             {title}
                                         </NavLink>
@@ -80,7 +80,7 @@ export default function Footer() {
                         Rights Reserved.
                     </Typography>
                     <div className="flex gap-4 text-blue-gray-900 sm:justify-center">
-                        <Typography as="a" href="#" className="opacity-80 transition-opacity hover:opacity-100 hover:text-blue-900">
+                        <Typography as="a" href="#" className="opacity-80 transition-opacity hover:opacity-100 hover:text-blue-900 footer-icon">
                             <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                 <path
                                     fill-rule="evenodd"
@@ -89,7 +89,7 @@ export default function Footer() {
                                 />
                             </svg>
                         </Typography>
-                        <Typography as="a" href="#" className="opacity-80 transition-opacity hover:opacity-100 hover:text-pink-700">
+                        <Typography as="a" href="#" className="opacity-80 transition-opacity hover:opacity-100 footer-icon hover:text-pink-700">
                             <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                 <path
                                     fill-rule="evenodd"
@@ -98,12 +98,12 @@ export default function Footer() {
                                 />
                             </svg>
                         </Typography>
-                        <Typography as="a" href="#" className="opacity-80 transition-opacity hover:opacity-100 hover:text-blue-700">
+                        <Typography as="a" href="#" className="opacity-80 transition-opacity hover:opacity-100 footer-icon hover:text-blue-700">
                             <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                 <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
                             </svg>
                         </Typography>
-                        <Typography as="a" href="#" className="opacity-80 transition-opacity hover:opacity-100">
+                        <Typography as="a" href="#" className="opacity-80 transition-opacity hover:opacity-100 footer-icon">
                             <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
                                 <path
                                     fill-rule="evenodd"
