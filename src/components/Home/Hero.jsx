d90d4e13dfbcd3cf77a57c4c6efac1eec9558878
Kabanda Kpanti Michael <you@example.com>
2023-07-15 21:51:26 +0000

    Page Reload to Top

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -3,23 +3,19 @@ import {
   Carousel,
   Typography,
   Button,
-  Dialog,
-  DialogFooter,
-  DialogHeader,
-  DialogBody,
-  Input,
-  Textarea,
 } from "@material-tailwind/react";
 import "../../main.css";
 import 'animate.css';
-import { XMarkIcon } from "@heroicons/react/24/solid";
 import backgroundImage from '../../assets/my.jpg';
+import { useNavigate } from 'react-router-dom';
 
 export default function Hero() {
+  const navigate = useNavigate();
 
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(!open);
-
+  const handleOpen = () => {
+    navigate('/contact');
+  };
+  
   return (
     <section className="flex items-center justify-center h-screen">
       <Carousel className="rounded-none">
@@ -53,40 +49,10 @@ export default function Hero() {
                   style={{ "--fade-delay": "2.5s" }}
 
                 >
-                  <Button onClick={handleOpen} size="lg" color="white" c>
+                  <Button onClick={handleOpen} size="lg" color="white">
                     Explore
                   </Button>
                 </div>
-
-                <Dialog open={open} handler={handleOpen}>
-                  <div className="flex items-center justify-between">
-                    <DialogHeader className="flex items-center justify-center">Message us</DialogHeader>
-                    <XMarkIcon className="mr-3 h-5 w-5" onClick={handleOpen} />
-                  </div>
-                  <DialogBody divider>
-                    <div className="grid gap-6">
-                      <Input label="Full Name" />
-                      <Input label="Email" />
-                      <Textarea label="Message" />
-                    </div>
-                  </DialogBody>
-                  <DialogFooter className="space-x-2">
-                    <Button
-                      variant="outlined"
-                      color="red"
-                      onClick={handleOpen}
-                    >
-                      close
-                    </Button>
-                    <Button
-                      variant="gradient"
-                      color="green"
-                      onClick={handleOpen}
-                    >
-                      send message
-                    </Button>
-                  </DialogFooter>
-                </Dialog>
               </div>
             </div>
           </div>
@@ -117,34 +83,10 @@ export default function Hero() {
               >
                 Embracing Technological Innovation to Drive Transformation and Success. Unleashing the Power of Technology to Propel Your Business Forward.
               </Typography>
-              <React.Fragment className="flex justify-center gap-2 first-button"
-              >
+              <React.Fragment className="flex justify-center gap-2 first-button">
                 <Button onClick={handleOpen} size="lg" color="white">
                   Innovate
                 </Button>
-                <Dialog
-                  open={open} handler={handleOpen}
-                >
-                  <div className="flex items-center justify-between">
-                    <DialogHeader className="flex items-center justify-center">Message us</DialogHeader>
-                    <XMarkIcon className="mr-3 h-5 w-5" onClick={handleOpen} />
-                  </div>
-                  <DialogBody divider>
-                    <div className="grid gap-6">
-                      <Input label="Full Name" />
-                      <Input label="Email" />
-                      <Textarea label="Message" />
-                    </div>
-                  </DialogBody>
-                  <DialogFooter className="space-x-2">
-                    <Button variant="outlined" color="red" onClick={handleOpen}>
-                      close
-                    </Button>
-                    <Button variant="gradient" color="green" onClick={handleOpen}>
-                      send message
-                    </Button>
-                  </DialogFooter>
-                </Dialog>
               </React.Fragment>
             </div>
           </div>
@@ -173,36 +115,10 @@ export default function Hero() {
               >
                 Empowering businesses through digital transformation, revolutionizing processes, and unlocking new opportunities for growth
               </Typography>
-              <React.Fragment className="flex justify-center gap-2 first-button"
-              >
-
+              <React.Fragment className="flex justify-center gap-2 first-button">
                 <Button onClick={handleOpen} size="lg" color="white">
                   Transform
                 </Button>
-
-                <Dialog
-                  open={open} handler={handleOpen}
-                >
-                  <div className="flex items-center justify-between">
-                    <DialogHeader className="flex items-center justify-center">Message us</DialogHeader>
-                    <XMarkIcon className="mr-3 h-5 w-5" onClick={handleOpen} />
-                  </div>
-                  <DialogBody divider>
-                    <div className="grid gap-6">
-                      <Input label="Full Name" />
-                      <Input label="Email" />
-                      <Textarea label="Message" />
-                    </div>
-                  </DialogBody>
-                  <DialogFooter className="space-x-2">
-                    <Button variant="outlined" color="red" onClick={handleOpen}>
-                      close
-                    </Button>
-                    <Button variant="gradient" color="green" onClick={handleOpen}>
-                      send message
-                    </Button>
-                  </DialogFooter>
-                </Dialog>
               </React.Fragment>
             </div>
           </div>
