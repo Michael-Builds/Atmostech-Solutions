b72d9be08195e4b6ce403125273d3f30b5c82f8f
Kabanda Kpanti Michael <michaelkpantiramp@gmail.com>
2023-07-28 11:04:35 +0000

    Changes made

--- src/components/Home/Partners.jsx
+++ src/components/Home/Partners.jsx
@@ -4,12 +4,12 @@ import second from '../../assets/Sanitation.png';
 import third from '../../assets/Tamani.png';
 import fourth from '../../assets/RCD.png';
 import fifth from '../../assets/Engineering.png';
-
+import sixth from '../../assets/Sanitation.png';
 
 export default function Partners() {
     return (
         <section id='partners' className="bg-white py-24 sm:py-32">
-            <h1 className="mb-2 team-header fade-in-bottom text-center mb-5">Our Partners</h1>
+            <h1 className=" team-header fade-in-bottom text-center mb-5">Our Partners</h1>
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
 
                 <h2 className="text-center partner-sub text-lg font-semibold leading-8 text-gray-900">
@@ -44,13 +44,20 @@ export default function Partners() {
                         width={170}
                         height={80}
                     />
-                    <img
-                        className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+                      <img
+                        className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
                         src={fifth}
-                        alt="Statamic"
+                        alt="SavvyCal"
                         width={170}
                         height={80}
                     />
+                      <img
+                        className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+                        src={sixth}
+                        alt="SavvyCal"
+                        width={170}
+                        height={80}
+                    />       
                 </div>
             </div>
         </section>
