4535617aeb52c226d4f3a4a7843861818443f5de
Kabanda Kpanti Michael <michaelkpantiramp@gmail.com>
2023-07-28 21:25:21 +0000

    Working on Chnages

--- src/components/NewsLetter/NewsLetter.jsx
+++ src/components/NewsLetter/NewsLetter.jsx
@@ -2,16 +2,15 @@ import { CalendarDaysIcon, HandRaisedIcon } from '@heroicons/react/24/outline'
 
 export default function NewsLetter() {
     return (
-        <div className="relative isolate overflow-hidden bg-green-700 py-16 sm:py-24 lg:py-32">
-            <div className="mx-auto max-w-7xl px-6 lg:px-8">
-                <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
+        <div className="relative py-16 overflow-hidden bg-green-700 isolate sm:py-24 lg:py-32">
+            <div className="px-6 mx-auto max-w-7xl lg:px-8">
+                <div className="grid max-w-2xl grid-cols-1 mx-auto gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
                     <div className="max-w-xl lg:max-w-lg">
                         <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Subscribe to our newsletter.</h2>
                         <p className="mt-4 text-lg leading-8 text-gray-300">
-                            Nostrud amet eu ullamco nisi aute in ad minim nostrud adipisicing velit quis. Duis tempor incididunt
-                            dolore.
+                        Stay updated with our latest news and exclusive offers by subscribing to our newsletter. Join our community today and never miss out on exciting updates!
                         </p>
-                        <div className="mt-6 flex max-w-md gap-x-4">
+                        <div className="flex max-w-md mt-6 gap-x-4">
                             <label htmlFor="email-address" className="sr-only">
                                 Email address
                             </label>
@@ -34,27 +33,27 @@ export default function NewsLetter() {
                     </div>
                     <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
                         <div className="flex flex-col items-start">
-                            <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
-                                <CalendarDaysIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                            <div className="p-2 rounded-md bg-white/5 ring-1 ring-white/10">
+                                <CalendarDaysIcon className="w-6 h-6 text-white" aria-hidden="true" />
                             </div>
                             <dt className="mt-4 font-semibold text-white">Weekly articles</dt>
                             <dd className="mt-2 leading-7 text-gray-400">
-                                Non laboris consequat cupidatat laborum magna. Eiusmod non irure cupidatat duis commodo amet.
+                            Explore a world of knowledge with our weekly newsletter featuring curated articles on diverse topics.
                             </dd>
                         </div>
                         <div className="flex flex-col items-start">
-                            <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
-                                <HandRaisedIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                            <div className="p-2 rounded-md bg-white/5 ring-1 ring-white/10">
+                                <HandRaisedIcon className="w-6 h-6 text-white" aria-hidden="true" />
                             </div>
                             <dt className="mt-4 font-semibold text-white">No spam</dt>
                             <dd className="mt-2 leading-7 text-gray-400">
-                                Officia excepteur ullamco ut sint duis proident non adipisicing. Voluptate incididunt anim.
+                            Subscribe now for our no-spam weekly articles, and receive a treasure trove of curated content delivered directly to your inbox.
                             </dd>
                         </div>
                     </dl>
                 </div>
             </div>
-            <div className="absolute left-1/2 top-0 -z-10 -translate-x-1/2 blur-3xl xl:-top-6" aria-hidden="true">
+            <div className="absolute top-0 -translate-x-1/2 left-1/2 -z-10 blur-3xl xl:-top-6" aria-hidden="true">
                 <div
                     className="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30"
                     style={{
