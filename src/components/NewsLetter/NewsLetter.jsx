d290854d4a34877694fe030c49bbb3d6d01d3c68
Kabanda Kpanti Michael <michaelkpantiramp@gmail.com>
2023-08-07 17:51:52 +0000

    Backend Successsfully Integrated

--- src/components/NewsLetter/NewsLetter.jsx
+++ src/components/NewsLetter/NewsLetter.jsx
@@ -1,6 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CalendarDaysIcon, HandRaisedIcon } from '@heroicons/react/24/outline'
+import { Alert } from "@material-tailwind/react";
+
+
+function Icon() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="h-6 w-6"
+        >
+            <path
+                fillRule="evenodd"
+                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                clipRule="evenodd"
+            />
+        </svg>
+    );
+}
+
+function ErrorIcon() {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="h-6 w-6 text-red-500"
+        >
+            <path
+                fillRule="evenodd"
+                d="M12 2c6.627 0 12 5.373 12 12s-5.373 12-12 12S0 20.627 0 14 5.373 2 12 2zm0 1c-5.523 0-10 4.477-10 10s4.477 10 10 10 10-4.477 10-10S17.523 3 12 3zm1 13h-2v-2h2v2zm0-4h-2V7h2v5z"
+                clipRule="evenodd"
+            />
+        </svg>
+    );
+}
+
 export default function NewsLetter() {
+    const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+    const [isFormError, setIsFormError] = useState(false);
+    const [email, setEmail] = useState('');
+
+    const handleChange = (e) => {
+      setEmail(e.target.value);
+    };
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('https://v1.nocodeapi.com/kpanti/google_sheets/hJoVtXWJDNiMyWHN?tabId=NewsLetter', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify([[email, new Date().toLocaleString()]]),
+      });
+
+      if (response.ok) {
+        setIsFormSubmitted(true);
+        setEmail('');
+        setIsFormError(false); // Reset form error state on successful submission
+      } else {
+        setIsFormError(true);
+      }
+    } catch (err) {
+      console.log(err);
+      setIsFormError(true);
+    }
+  };
+
+    // Modal component
+    const SuccessModal = () => (
+        <div className="modal">
+            <div className="modal-content">
+                <Alert
+                    icon={<Icon />}
+                    className="rounded-none mb-8 border-l-4 border-[#2ec946] bg-[#2ec946]/10 font-medium text-[#2ec946]"
+                >
+                    Newsletter subscription successful!
+                </Alert>
+                <button onClick={() => setIsFormSubmitted(false)}>Close</button>
+            </div>
+        </div>
+    );
 
     return (
         <div className="relative py-16 overflow-hidden bg-gray-900 isolate sm:py-24 lg:py-32">
@@ -16,7 +100,23 @@ export default function NewsLetter() {
                         <p className="mt-4 text-lg leading-8 text-gray-300">
                             Stay updated with our latest news and exclusive offers by subscribing to our newsletter. Join our community today and never miss out on exciting updates!
                         </p>
-                        <div className="flex max-w-md mt-6 gap-x-4" >
+                        <form className="flex flex-col max-w-md mt-6 gap-y-4" onSubmit={handleSubmit} >
+                            {isFormSubmitted && !isFormError && (
+                                <Alert
+                                    icon={<Icon />}
+                                    className="rounded-none mb-4 border-l-4 border-[#2ec946] bg-[#2ec946]/10 font-medium text-[#2ec946]"
+                                >
+                                    Newsletter subscription successful!
+                                </Alert>
+                            )}
+                            {isFormError && (
+                                <Alert
+                                    icon={<ErrorIcon />}
+                                    className="rounded-none mb-4 border-l-4 border-red-500 bg-red-50 font-medium text-red-500"
+                                >
+                                    An error occurred. Please try again later.
+                                </Alert>
+                            )}
                             <label htmlFor="email-address" className="sr-only">
                                 Email address
                             </label>
@@ -25,19 +125,20 @@ export default function NewsLetter() {
                                 name="email"
                                 type="email"
                                 autoComplete="email"
+                                value={email}
+                                onChange={handleChange}
                                 required
                                 className="min-w-0 flex-auto rounded-md border-0 bg-white px-3.5 py-2 text-black shadow-white ring-1 ring-inset ring-white focus:ring-2 focus:ring-inset focus:ring-gray-400 sm:text-sm sm:leading-6"
                                 placeholder="Enter your email"
                                 style={{ outline: 'none' }}
                             />
-
                             <button
                                 type="submit"
                                 className="flex-none rounded-md bg-gray-800 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-gray-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
                             >
                                 Subscribe
                             </button>
-                        </div>
+                        </form>
                     </div>
                     <dl id='news-main' className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
                         <div className="flex flex-col items-start">
@@ -70,7 +171,10 @@ export default function NewsLetter() {
                             'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
                     }}
                 />
-            </div>          
+            </div>
+            {/* Conditionally render the SuccessModal when isFormSubmitted is true */}
+            {isFormSubmitted && <SuccessModal />}
         </div>
+
     );
 };
