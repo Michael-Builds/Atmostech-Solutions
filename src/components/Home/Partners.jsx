e5782873ee0f517547c450ff8c025dac2a36602b
Kabanda Kpanti Michael <michaelkpantiramp@gmail.com>
2023-09-26 13:39:29 +0000

    Updated Team Images

--- src/components/Home/Partners.jsx
+++ src/components/Home/Partners.jsx
@@ -19,7 +19,7 @@ export default function Partners() {
       <h1 className="mb-5 text-center  team-header fade-in-bottom">Our Partners</h1>
       <div className="px-6 mx-auto max-w-7xl lg:px-8">
         <h2 className="text-lg font-semibold leading-8 text-center text-gray-900 partner-sub">
-          Trusted by the world’s most innovative teams
+          Trusted by the worlds most innovative teams
         </h2>
         <div className="grid items-center max-w-lg grid-cols-4 mx-auto mt-12 gap-x-4 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
           {partnerData.map((partner, index) => (
