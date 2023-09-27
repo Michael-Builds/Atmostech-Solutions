cf8be661748e3445421e033b612903dc1be6de38
Kabanda Kpanti Michael <michaelkpantiramp@gmail.com>
2023-09-27 12:56:08 +0000

    Updated Testimonial

--- src/components/About/Testimonial.jsx
+++ src/components/About/Testimonial.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import team from "../../assets/team1.png";
-import testimonial from './mishel.png';
+import Vic from './vic.jpeg';
+import logo from './logo.png';
 
 export default function Testimonial() {
     return (
@@ -8,25 +8,26 @@ export default function Testimonial() {
             <div className="absolute inset-0 -z-10 bg-[radial-gradient(45rem_50rem_at_top,theme(colors.indigo.100),white)] opacity-20" />
             <div className="absolute inset-y-0 right-1/2 -z-10 mr-16 w-[200%] origin-bottom-left skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-green-100 sm:mr-28 lg:mr-0 xl:mr-16 xl:origin-center" />
             <div className="max-w-2xl mx-auto lg:max-w-4xl">
-                <img  className="h-24 mx-auto w-34 testimonial-image" src={testimonial} alt="" />
+                <img className="h-24 mx-auto w-34 testimonial-image" src={logo} alt="" />
                 <figure className="mt-10">
-                    <blockquote  className="mt-2 text-xl font-semibold leading-8 text-center text-gray-900 sm:text-2xl sm:leading-9">
+                    <blockquote className="mt-2 text-lg font-medium leading-8 text-center text-gray-900 sm:text-1xl sm:leading-9">
                         <p >
-                            “Exceptional service and innovative solutions – this tech company exceeded all our expectations. Their expertise and dedication have been instrumental in driving our success”
+                            “Thanks to ATMOSTECH @atmostechsolutions, my energy to execute tasks has skyrocketed! Mr. SESHIE and his team provided me with an incredible IT system that's been a game-changer for my entrepreneurial journey.
+                            Their customized system is a must-have for SMEs. Reach out to Chris directly @ranzymore to experience the difference. Afeto, AKPEEEEEE!”
                         </p>
                     </blockquote>
                     <figcaption className="mt-10">
                         <img
-                            className="w-10 h-10 mx-auto bg-gray-500 rounded-full"
-                            src={team}
+                            className="w-34 mx-auto h-32 bg-gray-500 rounded-lg"
+                            src={Vic}
                             alt=""
                         />
                         <div className="flex items-center justify-center mt-4 space-x-3 text-base">
-                            <div className="font-semibold text-gray-900">Kabanda Michael</div>
+                            <div className="font-semibold text-gray-900">Victoria Sehsie</div>
                             <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
                                 <circle cx={1} cy={1} r={1} />
                             </svg>
-                            <div className="text-gray-600">CEO of Mishel's Inc.</div>
+                            <div className="text-gray-600">CEO & Founder of Smart Kids Palace.</div>
                         </div>
                     </figcaption>
                 </figure>
