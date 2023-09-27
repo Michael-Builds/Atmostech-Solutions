a50f3e79cf98f40c34a7d8e3436681a26a85ed4c
Kabanda Kpanti Michael <michaelkpantiramp@gmail.com>
2023-09-27 13:03:42 +0000

    Updated Testimonial

--- src/components/About/Testimonial.jsx
+++ src/components/About/Testimonial.jsx
@@ -27,7 +27,7 @@ export default function Testimonial() {
                             <svg viewBox="0 0 2 2" width={3} height={3} aria-hidden="true" className="fill-gray-900">
                                 <circle cx={1} cy={1} r={1} />
                             </svg>
-                            <div className="text-gray-600">CEO & Founder of Smart Kids Palace.</div>
+                            <div className="text-gray-600">CEO & Founder of Smart Kidz Palace.</div>
                         </div>
                     </figcaption>
                 </figure>
